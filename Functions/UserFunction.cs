d6bff049f25b6e36eca6e7aa7cf320207dde8bed
ByronKruger <byron@synerics.com>
2026-06-08 18:16:41 +0200

    2nd major initial commit

---
+++ Functions/UserFunction.cs
@@ -0,0 +1,50 @@
+using Coffeeg.Dtos.User;
+using Coffeeg.Interfaces.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace TestFunctionApp.Functions
+{
+    public class UserFunction(IUserManagementService Service)
+    {
+        //private readonly ILogger<UserFunction> _logger;
+
+        //public UserFunction(ILogger<UserFunction> logger)
+        //{                                                                                                    
+        //    _logger = logger;
+        //}
+
+        //[Function("Function1")]
+        //public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        //{
+        //    _logger.LogInformation("C# HTTP trigger function processed a request.");
+        //    return new OkObjectResult("Welcome to Azure Functions!123");
+        //}
+
+        [Function("Register")]
+        public async Task<IActionResult> Register(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "api/user/register")]
+            HttpRequest req)//,
+            //[FromBody] RegisterUser user)
+        {
+            RegisterUser? user = null;
+            try
+            {
+                user = await req.ReadFromJsonAsync<RegisterUser>();
+            }
+            catch (Exception ex)
+            {
+                // Log the deserialization error
+                return new BadRequestObjectResult($"Invalid JSON: {ex.Message}");
+            }
+
+            var result = await Service.CreateUserAsync(user);
+
+            if (!result.IsSuccess) return new BadRequestObjectResult("Could not register");
+
+            return new OkObjectResult(result.Value);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,27 @@
+using Coffeeg.Extensions;
+using Coffeeg.Helpers.AutoMapperProfiles;
+using Coffeeg.Interfaces.Repositories;
+using Coffeeg.Interfaces.Services;
+using Coffeeg.Services;
 using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TestFunctionApp.Data;
+using TestFunctionApp.Repositories;
 
 var builder = FunctionsApplication.CreateBuilder(args);
 
 builder.ConfigureFunctionsWebApplication();
+
+builder.Services.AddDataContext<CoffeegDbContext>(builder.Configuration);
+builder.Services.AddCoffeegIdentityCore<CoffeegDbContext>();
+
+builder.Services.AddScoped<IUserManagementService, UserManagementService>();
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+builder.Services.AddAutoMapper(typeof(BeverageCustomisationProfiles));   // scans for profiles in that assembly
+builder.Services.AddAutoMapper(typeof(UserProfile));   // scans for profiles in that assembly
 
 // Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
 // builder.Services
