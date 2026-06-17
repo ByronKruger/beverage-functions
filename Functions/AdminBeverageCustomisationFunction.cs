26d989ec593ba694d290b4f73144f3d2ca0dc167
ByronKruger <byron@synerics.com>
2026-06-17 20:43:06 +0200

    Added admin endpoints for beverage-type and ingredients

--- Functions/AdminBeverageCustomisationFunction.cs
+++ Functions/AdminBeverageCustomisationFunction.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using Coffeeg.Dtos.BeverageCustomisation;
+using Coffeeg.Interfaces.Services;
+using Coffeeg.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +12,54 @@ using System.Threading.Tasks;
 
 namespace TestFunctionApp.Functions
 {
-    internal class AdminBeverageCustomisationFunction
+    public class AdminBeverageCustomisationFunction(IAdminBeverageManagementService Service)
     {
+        [Function("AddBeverageType")]
+        
+        public async Task<IActionResult> CreateBeverageType(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "beverage-admin/add-beverage-type")] 
+            [Microsoft.Azure.Functions.Worker.Http.FromBody] AddBeverageType dto)
+        {
+            // 1.
+            var result = await Service.CreateBeverageType(dto);
+
+            if (result.IsSuccess)
+            {
+                return new OkObjectResult("");
+            }
+            else
+            {
+                return new BadRequestObjectResult(new { error = result.ErrorMessage });
+            }
+
+            // 2.
+            //var result = await Service.CreateBeverageType(description);
+
+            //if (result.IsSuccess)
+            //    return CreatedAtAction(
+            //    nameof(GetBeverageType),           // ← prefer a real GET action name
+            //    new { id = result.Value.Id },      // route values (must match route template of the GET action)
+            //    result.Value                       // response body (usually the created DTO)
+            //);
+            //else
+            //    return BadRequest(new { error = result.ErrorMessage });
+        }
+
+        [Function("AddIngredients")]
+        public async Task<IActionResult> CreateIngredients(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "beverage-admin/add-ingredients")]
+            [Microsoft.Azure.Functions.Worker.Http.FromBody] AddIngredients dto)
+        {
+            var result = await Service.CreateIngredients(dto);
+
+            if (result.IsSuccess)
+            {
+                return new OkObjectResult("");
+            }
+            else
+            {
+                return new BadRequestObjectResult(new { error = result.ErrorMessage });
+            }
+        }
     }
 }
